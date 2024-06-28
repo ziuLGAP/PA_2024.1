8e14cf24a5490d3235d29f45cbe2dcad40aa59c7
Luiz G. A. Pires <88043239+ziuLGAP@users.noreply.github.com>
2024-06-28 19:11:02 -0300

    Update singleton.js

--- Singleton/singleton.js
+++ Singleton/singleton.js
@@ -1,1 +1,24 @@
-a
+function Process(state){
+  this.state = state
+}
+
+const Singleton = (function(){
+  function ProcessManager(){
+    this.numProcess = 0
+  }
+
+  let pManager
+
+  function createProcessManager(){
+    pManager = new ProcessManager()
+    return pManager
+  }
+
+  return{
+    getProcessManager:()=>{
+      if(!pManager)
+        pManager = createProcessManager()
+      return pManager
+    }
+  }  
+})()
