abb6f033e28b65519b7fe9459ddf27a7dbe1b5ec
Luiz G. A. Pires <luizapires@poli.ufrj.br>
2024-06-28 21:25:14 -0300

    Updata iike.js

--- IIKE/iike.js
+++ IIKE/iike.js
@@ -1,1 +1,20 @@
+const Singleton = (function(){
+    function ProcessManager(){
+        this.numProcess = 0
+    }
 
+    let pManager
+
+    function createProcessManager(){
+        pManager = new ProcessManager()
+        return pManager
+    }
+
+    return {
+        getProcessManager:()=>{
+            if(!pManager)
+                pManager = createProcessManager()
+            return pManager
+        }
+    }
+})()
