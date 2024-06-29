cf63813156aa74af5d8d0ac94f4571101ca48786
Luiz G. A. Pires <luizapires@poli.ufrj.br>
2024-06-28 21:35:09 -0300

    Update observer.js

--- Observer/observer.js
+++ Observer/observer.js
@@ -1,1 +1,36 @@
-a
+function Subject(){
+    this.observers = []
+}
+
+Subject.prototype = {
+    subscribe: function(fn){
+        this.observers.push(fn)
+    },
+    unsubscribe: function(fnToRemove){
+        this.observers = this.observers.filter(fn => {
+            if(fn != fnToRemove)
+                return fn
+        })
+    },
+    fire: function(){
+        this.observers.forEach(fn => {
+            fn.call()
+        })
+    }
+}
+
+const subject = new Subject()
+
+function Observer1(){
+    console.log("Observer 1!")
+}
+
+function Observer2(){
+    console.log("Observer 2!")
+}
+
+subject.subscribe(Observer1)
+subject.subscribe(Observer2)
+subject.unsubscribe(Observer2)
+
+subject.fire()
